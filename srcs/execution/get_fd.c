54b0da11ebb675fc90ff2657b8450419ec52eba7
Pierre Anger <panger@z4r6p3.42lyon.fr>
2024-01-05 18:28:11 +0100

    get_fd_block to do

--- srcs/execution/get_fd.c
+++ srcs/execution/get_fd.c
@@ -6,24 +6,34 @@
 /*   By: panger <panger@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/05 14:45:19 by panger            #+#    #+#             */
-/*   Updated: 2024/01/05 17:01:26 by panger           ###   ########.fr       */
+/*   Updated: 2024/01/05 18:27:52 by panger           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	get_in_fd(t_token *start)
+int	get_fd_block(int fd[2], t_token *start)
 {
-	int	fd;
-
-	fd = -1;
+	t_token *last_in;
+	t_token *last_out;
+	
+	fd[0] = -1;
+	fd[1] = -1;
+	last_in = NULL;
+	last_out = NULL;
 	while (start->prev && start->prev->type != PIPE)
 		start = start->prev;
 	while (start->next && start->next->type != PIPE)
 	{
-		
+		if (start->type == REDIRECT_IN)
+			last_in = start->next;
+		if (start->type == REDIRECT_OUT || start->type == REDIRECT_APPEND)
+			last_out = start->next;
 		start = start->next;
 	}
+	fd[IN] = open_file(last_in);
+	fd[OUT] = open_file(last_out);
+	return (fd);
 }
 
 int	get_fd(int fd[4], t_token *input, int i, char *cmd)
@@ -35,6 +45,5 @@ int	get_fd(int fd[4], t_token *input, int i, char *cmd)
 		input = input->next;
 	if (!input)
 		return (-1);
-	fd[2 + IN] = get_in_fd(input);
-	fd[2 + OUT] = get_out_fd(input);
+	get_fd_block(&fd[2], input);
 }
